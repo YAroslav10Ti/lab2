c0f594f5b21aec23bfc88ffe34242ccf22941fb0
YAroslav10Ti <yaroslavtitov10@gmail.com>
2025-05-17 16:07:51 +0000

    Add code comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
+// Main function: asks for user's name and prints a greeting
 int main() {
-    std::string name;
+    std::string name;  // Stores the user's name
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
